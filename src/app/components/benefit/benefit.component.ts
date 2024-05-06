d2c04c1809b1fbc97ab240cc7170ca97ab17ff3c
savio <saviopf22@gmail.com>
2024-05-06 17:33:42 -0300

    primeira sessão da home

---
+++ src/app/components/benefit/benefit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-benefit',
+  standalone: true,
+  imports: [],
+  templateUrl: './benefit.component.html',
+  styleUrl: './benefit.component.scss'
+})
+export class BenefitComponent {
+
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public benefits = [
+    {
+      title: 'Equipe capacitada',
+      imagePath: '/assets/team.png',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur.',
+    },
+    {
+      title: 'Seus projetos guiados do zero',
+      imagePath: '/assets/project-guide.png',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur.',
+    },
+  ];
+}
